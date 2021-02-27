ef899f54cafb1d9c01d544b98c38bc9570bb2346
Julian Lau <julian.lau@aol.com>
2021-02-27 19:10:08 +0100

    color

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   pwa: {
     name: 'MyNotes',
-    themeColor: '#0f1820',
+    themeColor: '#003840',
     msTileColor: '#0f1820',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '0f1820',
+    appleMobileWebAppStatusBarStyle: '#0f1820',
     start_url: '.',
 
     // configure the workbox plugin
