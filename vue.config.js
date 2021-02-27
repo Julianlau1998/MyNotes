ca191044a639c92b551d8b9b5b56be86e02cceaa
Julian Lau <julian.lau@aol.com>
2021-02-27 18:34:13 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -5,18 +5,12 @@ module.exports = {
     msTileColor: '#0f1820',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: '0f1820',
+    start_url: '.',
 
     // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
      workboxOptions: {
       swSrc: './public/service-worker.js',
-
-      manifestOptions: {
-        start_url: '.',
-        name: 'MyNotes',
-        themeColor: '#0f1820',
-        display: 'standalone',
-      }
     }
   }
 }
