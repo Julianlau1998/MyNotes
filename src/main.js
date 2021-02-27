397af2fbc97581cd27a745cab29e2028242b2347
Julian Lau <julian.lau@aol.com>
2021-02-27 20:41:00 +0100

    service worker

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import wb from "./registerServiceWorker";
-
-Vue.prototype.$workbox = wb;
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false
+
 import uuid from '@estudioliver/vue-uuid-v4'
 Vue.use(uuid)
 
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -30,6 +30,3 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
-else {
-  alert('test')
-}
