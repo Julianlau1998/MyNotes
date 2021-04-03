76c61d91de3c68af816dcd0b9ce4e7935b20c3b0
Julian Lau <julian.lau@aol.com>
2021-04-04 00:37:05 +0200

    share position

--- src/views/List.vue
+++ src/views/List.vue
@@ -482,7 +482,7 @@ input[type="checkbox"] {
 #share {
     width: 5rem;
     position: absolute;
-    top: -1rem;
+    top: -1.1rem;
     left: 50%;
     transform: translateX(-50%);
     cursor: pointer;
