57c632eb63f3318688d5b37486e1a2905aebd1e0
Julian Lau <julian.lau@aol.com>
2021-04-16 21:59:20 +0200

    meta

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,6 @@
 </template>
 
 <script>
-if (window.location.protocol.indexOf('https') == 0){
-  var el = document.createElement('meta')
-  el.setAttribute('http-equiv', 'Content-Security-Policy')
-  el.setAttribute('content', 'upgrade-insecure-requests')
-  document.head.append(el)
-}
 export default {
   data () {
     return {
