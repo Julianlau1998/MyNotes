7efb44f211da1af9f220d464a001a18989ea66cc
Julian Lau <julian.lau@aol.com>
2021-04-27 18:00:45 +0200

    server ip

--- MyNotes/src/store/index.js
+++ MyNotes/src/store/index.js
@@ -16,8 +16,8 @@ export default new Vuex.Store({
     currentComponent: localStorage.getItem('currentComponent'),
     currentFolder: '',
     dragging: false,
-    // localhost: 'http://139.162.158.148:1323/api/'
-    localhost: 'http://192.168.178.58:1323/api/',
+    localhost: 'http://139.162.158.148:1323/api/',
+    // localhost: 'http://192.168.178.58:1323/api/',
     folder: ''
   },
   mutations: {
